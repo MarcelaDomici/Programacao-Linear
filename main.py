437da6a6da2f9c8e3cf58f34a3267801172f6ea4
João Guilherme <joaorato58@gmail.com>
2025-09-24 21:10:35 -0300

    feat: servidor FastAPI

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, FileResponse
+from metodosbasicos import gerar_matriz_restricao, gerar_solucao_inicial, avalia_escala
+
+app = FastAPI()
+
+@app.get("/api/gerar_escala")
+def gerar_escala_endpoint(tipo: str = "F"):
+
+    matriz_restricao = gerar_matriz_restricao(tipo)
+    escala = gerar_solucao_inicial(matriz_restricao)
+    avaliacao = avalia_escala(escala)
+
+    return {
+        "escala": escala,
+        "avaliacao": avaliacao,
+    }
+
+#Endpoints para as páginas HTML
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return FileResponse("menu.html")
+
+@app.get("/metodoBasico.html", response_class=HTMLResponse)
+async def metodo_basico():
+    return FileResponse("metodoBasico.html")
+
+@app.get("/algoritmoGenetico.html", response_class=HTMLResponse)
+async def algoritmo_genetico():
+    return FileResponse("algoritmoGenetico.html")
+
+@app.get("/sobre.html", response_class=HTMLResponse)
+async def sobre():
+    return FileResponse("sobre.html")
+
+@app.get("/menu.html", response_class=HTMLResponse)
+async def home():
+    return FileResponse("menu.html")
+
+#Arquivos estáticos
+app.mount("/CSS", StaticFiles(directory="CSS"), name="css")
